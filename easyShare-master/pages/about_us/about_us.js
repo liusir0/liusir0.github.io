b81e5fb5d7ec386b78b55f83c77fbf61db536943
liusir0 <2641630426@qq.com>
2022-11-14 10:20:43 +0800

    test
    表白墙小程序

---
+++ easyShare-master/pages/about_us/about_us.js
@@ -0,0 +1,29 @@
+// pages/about_us/about_us.js
+Page({
+  data: {
+
+  },
+
+
+  /**
+   * 测试按钮
+   */
+  test(){
+  
+
+  },
+
+
+
+
+
+  onLoad: function (options) {
+
+
+  },
+
+  onShow: function () {
+
+  },
+
+})
